11905fec7131d55dac3eaa157da8109ed65e23ce
AmySimps1 <simpson.r.amy@gmail.com>
2020-12-15 19:22:41 +0000

    unimportant

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
       <header>
         <h2><a href="https://recipemovieclub.herokuapp.com/">Recipe Movie Club</a></h2>
         <nav>
-          <li><a href="https://recipemovieclub.herokuapp.com/">New Recipe</a></li>
+          <li><a href="https://recipemovieclub.herokuapp.com /">New Recipe</a></li>
           <li><a href="https://recipemovieclub.herokuapp.com/">Home</a></li>
           <li><a href="https://recipemovieclub.herokuapp.com/">About</a></li>
           <li><a href="https://recipemovieclub.herokuapp.com/">Contact Us</a></li>
