73a4cfb491c9e6681f9f7e212b7e2ce8c41d0522
AmySimps1 <simpson.r.amy@gmail.com>
2020-12-16 00:21:55 +0000

    recipe form added, delete recipe

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './Navbar.css';
 
 class Navbar extends Component {
+  static defaultProps = {
+    onNewRecipe() {}
+  }
+  
+  static propTypes = {
+    onNewRecipe: PropTypes.func
+  }
+
   render() {
     return (
       <header>
         <h2><a href="https://recipemovieclub.herokuapp.com/">Recipe Movie Club</a></h2>
         <nav>
-          <li><a href="https://recipemovieclub.herokuapp.com/">New Recipe</a></li>
+		  <li><button onClick={this.props.onNewRecipe}>New Recipe</button></li>
           <li><a href="https://recipemovieclub.herokuapp.com/">Home</a></li>
           <li><a href="https://recipemovieclub.herokuapp.com/">About</a></li>
           <li><a href="https://recipemovieclub.herokuapp.com/">Contact Us</a></li>
